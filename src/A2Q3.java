b2d7198afeee2e9d3eaf217541141528dd24f1dd
ratid6445 <ratid6445@HRH-IC0027322.wrdsb.ca>
2017-02-27 09:19:39 -0500

    Assignment 2

--- src/A2Q3.java
+++ src/A2Q3.java
@@ -20,62 +20,39 @@ public class A2Q3 {
     public static void main(String[] args) {
         // create city
         City kw = new City();
-        // create robot
-        Robot karel = new Robot(kw, 3, 1, Direction.NORTH);
-        // create castle
-        new Wall(kw, 2, 2, Direction.NORTH);
-        new Wall(kw, 2, 3, Direction.NORTH);
-        new Wall(kw, 2, 3, Direction.EAST);
-        new Wall(kw, 3, 3, Direction.EAST);
-        new Wall(kw, 3, 3, Direction.SOUTH);
-        new Wall(kw, 3, 2, Direction.SOUTH);
-        new Wall(kw, 3, 2, Direction.WEST);
-        new Wall(kw, 2, 2, Direction.WEST);
-
-        new Wall(kw, 1, 1, Direction.NORTH);
-        new Wall(kw, 1, 1, Direction.EAST);
-        new Wall(kw, 1, 1, Direction.SOUTH);
-        new Wall(kw, 1, 1, Direction.WEST);
-
-        new Wall(kw, 4, 1, Direction.NORTH);
-        new Wall(kw, 4, 1, Direction.EAST);
-        new Wall(kw, 4, 1, Direction.SOUTH);
-        new Wall(kw, 4, 1, Direction.WEST);
-
-        new Wall(kw, 4, 4, Direction.NORTH);
-        new Wall(kw, 4, 4, Direction.EAST);
-        new Wall(kw, 4, 4, Direction.SOUTH);
-        new Wall(kw, 4, 4, Direction.WEST);
-
-        new Wall(kw, 1, 4, Direction.NORTH);
-        new Wall(kw, 1, 4, Direction.EAST);
-        new Wall(kw, 1, 4, Direction.SOUTH);
-        new Wall(kw, 1, 4, Direction.WEST);
-
-        //move robot around castle (repetitive)
-        while (true) {
-            if (karel.frontIsClear() == true) {
+        //create robot
+        Robot karel = new Robot(kw, 4, 8, Direction.WEST);
+        //create algorithm
+            
+            if (karel.getDirection() == Direction.EAST) {
+                karel.turnLeft();
+                karel.turnLeft();
+            }if (karel.getDirection() == Direction.NORTH) {
+                karel.turnLeft();
+            }if (karel.getDirection() == Direction.WEST) {
                 karel.move();
-            }
-            if (karel.frontIsClear() == false) {
-                karel.turnLeft();
-                karel.move();
+            }if (karel.getDirection() == Direction.SOUTH) {
                 karel.turnLeft();
                 karel.turnLeft();
                 karel.turnLeft();
-                karel.move();
-                karel.move();
-                karel.turnLeft();
-                karel.turnLeft();
-                karel.turnLeft();
-                karel.move();
-                karel.move();
-                karel.turnLeft();
-                karel.turnLeft();
-                karel.turnLeft();
-                karel.move();
-                karel.turnLeft();
-            }
-        }
+            }while(karel.getAvenue() >0){
+                    karel.move();
+            }while(karel.getStreet() >0){
+                        if (karel.getDirection()==Direction.WEST){
+                        karel.turnLeft();
+                        karel.turnLeft();
+                        karel.turnLeft();
+                  
+                        }
+                        karel.move();
+                    }
+            
+                }
     }
-}
+
+
+
+    
+
+
+
